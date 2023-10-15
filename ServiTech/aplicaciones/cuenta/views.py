9d59945f067297b12156495ee47aca014424e23c
JGer04 <134817299+JGer04@users.noreply.github.com>
2023-10-15 17:14:21 -0600

    Catalogo agregado

--- ServiTech/aplicaciones/cuenta/views.py
+++ ServiTech/aplicaciones/cuenta/views.py
@@ -1,5 +1,15 @@
 from django.shortcuts import render
+from.models import cuenta
+from django.views.generic import CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
+from django.contrib.messages.views import SuccessMessageMixin
 
 # Create your views here.
-def cuenta(request):
-    return render(request, 'cuenta/cuenta.html')
+def cuentas(request):
+    catalogo = cuenta.objects.all().order_by('id')
+
+    return render(request, 'cuenta/cuenta.html',{
+        'catalogo':catalogo
+    })
+
+
