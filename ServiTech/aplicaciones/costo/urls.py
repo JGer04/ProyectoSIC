879177b1b339cfa3bf88a095fb4ec99246ab0901
Eduardo-LopezRodriguez <lr21008@ues.edu.sv>
2023-10-24 08:22:01 -0600

    creación de vistas en costeo

--- ServiTech/aplicaciones/costo/urls.py
+++ ServiTech/aplicaciones/costo/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path('costo/costo', views.reporte, name = 'costo'),
+    path('costo/servicio', views.servicio, name = 'servicio'),
+    path('index/actividad', views.actividad, name='actividad'),
+    path('index/costeo', views.costeo, name='costeo'),
 ]
--- ServiTech/aplicaciones/costo/views.py
+++ ServiTech/aplicaciones/costo/views.py
@@ -3,3 +3,12 @@ from django.shortcuts import render
 # Create your views here.
 def reporte(request):
     return render(request, 'costo/costo.html')
+
+def servicio(request):
+    return render(request, 'costo/servicio.html')
+
+def actividad(request):
+    return render(request, 'costo/actividad.html')
+
+def costeo(request):
+    return render(request, 'costo/costeo.html')
